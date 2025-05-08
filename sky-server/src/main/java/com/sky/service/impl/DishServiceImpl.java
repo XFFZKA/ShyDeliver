eab1ae21ecccfb36d254a69e16be9a1b9cc4e762
XFFZKA <3201455355@qq.com>
2025-05-08 22:03:03 +0800

    分页查询

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.CategoryMapper;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +47,15 @@ public class DishServiceImpl implements DishService {
         }
 
 
+    }
+
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO queryDTO) {
+
+        PageHelper.startPage(queryDTO.getPage(),queryDTO.getPageSize());
+
+        Page<DishVO> page = dishMapper.pageQuery(queryDTO);
+
+        return new PageResult(page.getTotal(),page.getResult());
     }
 }
